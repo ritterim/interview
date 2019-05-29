c383fd9d99efe795988a1633c4ef88a46a75fb84
Seth Tum <stum@think3-innovations.com>
2019-05-29 18:24:42 -0400

    Interview Demo for Seth Tum

---
+++ InterviewDemo/Default.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="InterviewDemo._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>RIMdev Technical Interview</h1>
+        <p class="lead">
+            The purpose of this exercise is to understand an applicants ability to problem solve, think critically, and write software with existing RIMdev team members. While there is no expected answer, we do expect the production of a working sample. That means the work must compile, transpile, or interpret and above all else run with no immediate exceptions.
+            <br />
+
+        </p>
+        <p>While accomplishing this exercise is permitted before interviewing at Ritter Insurance Marketing, it may also be completed in collaboration with the interviewers.</p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Objectives</h2>
+            <p>
+                Import data.json into memory
+                
+ 
+            </p>
+            <p>
+                Submit an application that answers the following questions about the data set:
+            </p>
+        </div>
+
+
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Answers</h2>
+            <p id="q1" runat="server">What is the count of individuals over the age of 50? </p>
+            <p id="q2" runat="server">Who is last individual that registered who is still active? </p>
+            <p id="q3" runat="server">What are the counts of each favorite fruit? </p>
+            <p id="q4" runat="server">What is the most common eye color? </p>
+            <p id="q5" runat="server">What is the total balance of all individuals combined? </p>
+            <p id="q6" runat="server">What is the full name of the individual with the id of 5aabbca3e58dc67745d720b1 in the format of lastname, firstname? </p>
+
+        </div>
+
+    </div>
+     <div class="row">
+        <div class="col-md-12">
+            <h2>Documentation</h2>
+            <p>
+               I created a json file from the data set and created an embedded resource within the project.  
+               I then created 2 classes to convert the JSON string to a strongly typed class object and finally used multiple linq queries to filter the data set to answer the questions.
+ 
+            </p>
+           
+        </div>
+
+
+    </div>
+</asp:Content>
---
+++ InterviewDemo/Default.aspx.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace InterviewDemo
+{
+    public partial class _Default : Page
+    {
+        static HttpClient client = new HttpClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadJson();
+        }
+        public void LoadJson()
+        {
+
+            string filepath = Server.MapPath("~/data.json");
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                string json = r.ReadToEnd();
+                List<profile> items = JsonConvert.DeserializeObject<List<profile>>(json);
+
+                //Question 1 
+                var lstAge = (from x in items
+                              where x.age > 50
+                              select x).ToList();
+                q1.InnerHtml += "<span class=\"bold\">" + lstAge.Count.ToString() + " total individuals are over the age of 50." + "</span>";
+                //Question 2
+                var lstLastActive = (from x in items
+                                     orderby x.registered descending
+                                     select x).First();
+                q2.InnerHtml += "<span class=\"bold\">" + lstLastActive.name.first + " " + lstLastActive.name.last + " registered on " + lstLastActive.registered.ToShortDateString() + "</span>";
+                //Question 3
+                var lstFavFruit = (from x in items
+                                   group x.favoriteFruit by x.favoriteFruit into g
+                                   select new { Fruit = g.Key, CNT = g.Count() }).ToList();
+
+                string list = string.Empty;
+                for (int i = 0; i < lstFavFruit.Count; i++)
+                {
+                    list += lstFavFruit[i].Fruit + ":" + lstFavFruit[i].CNT.ToString() + " ";
+                }
+                q3.InnerHtml += "<span class=\"bold\">" + list + "</span>";
+                //Question 4
+                var lstCommonEyeColor = (from x in items
+                                         group x.eyeColor by x.eyeColor into g
+                                         orderby g.Count() descending
+                                         select g).ToList();
+                q4.InnerHtml += "<span class=\"bold\">" + lstCommonEyeColor[0].Key + ":" + lstCommonEyeColor[0].Count().ToString() + "</span>";
+                //Question 5
+                double balance = 0;
+                foreach (profile p in items)
+                {
+
+                    var t = double.Parse(p.balance, NumberStyles.Currency);
+                    balance += t;
+
+                }
+                q5.InnerHtml += "<span class=\"bold\">" + balance.ToString("C0") + "</span>";
+                //Question 6
+                var lstFullName = (from x in items
+                                   where x.id == "5aabbca3e58dc67745d720b1"
+                                   select x).First();
+
+
+                string fullName = lstFullName.name.last + "," + lstFullName.name.first;
+
+                q6.InnerHtml += "<span class=\"bold\">" + fullName + "</span>";
+
+            }
+        }
+    }
+    public class profile
+    {
+
+
+        public string favoriteFruit { get; set; }
+        public string greeting { get; set; }
+        public float longitude { get; set; }
+        public float latitude { get; set; }
+        public DateTime registered { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string company { get; set; }
+        public fullname name { get; set; }
+        public string eyeColor { get; set; }
+        public int age { get; set; }
+        public string balance { get; set; }
+        public bool isActive { get; set; }
+        public string id { get; set; }
+    }
+
+    public class fullname
+    {
+
+        public string last { get; set; }
+        public string first { get; set; }
+
+    }
+}
