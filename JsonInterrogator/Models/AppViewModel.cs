8f87a24529a116d1743edcea29058ad146a62b73
Daniel Vo <dan@daniel-vo.com>
2020-07-14 18:57:30 -0500

    transformed fruit data into a fruit report and mapped to view

--- JsonInterrogator/Models/AppViewModel.cs
+++ JsonInterrogator/Models/AppViewModel.cs
@@ -13,11 +13,26 @@ namespace JsonInterrogator.Models
         }
         public int CountOverAge50 { get; private set; }
         public Person LastActivePerson { get; private set; }
+        public IEnumerable<ReportViewModel> FruitReport { get; private set; }
 
         private void BuildViewModel()
         {
             this.CountOverAge50 = this._people.Where(x => x.Age > 50).Count();
             this.LastActivePerson = this._people.Last(x => x.IsActive);
+            this.FruitReport = this._people.GroupBy(x => x.FavoriteFruit)
+                .Select(x => new ReportViewModel(x.Key, x.Count()));
         }
+    }
+
+    public class ReportViewModel
+    {
+        public ReportViewModel(string key, int count)
+        {
+            this.Description = key;
+            this.Count = count;
+        }
+
+        public string Description { get; private set; }
+        public int Count { get; private set; }
     }
 }
