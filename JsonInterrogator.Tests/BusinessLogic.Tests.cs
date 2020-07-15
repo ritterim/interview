4d62a427fd72fdc0b07c30d0e3a97a2857cb53a5
Daniel Vo <dan@daniel-vo.com>
2020-07-14 19:12:42 -0500

    first fail, count over age 50 buisness logic

---
+++ JsonInterrogator.Tests/BusinessLogic.Tests.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using JsonInterrogator.Models;
+using NUnit.Framework;
+
+namespace JsonInterrogator.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            var people = new List<Person>
+            {
+                new Person { Age = 20 },
+                new Person { Age = 50 },
+                new Person { Age = 51 },
+                new Person { Age = 52 },
+            };
+
+            // Act
+            var result = people.GetCountOverAge50();
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+    }
+}
